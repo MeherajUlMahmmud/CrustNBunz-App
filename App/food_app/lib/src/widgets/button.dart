d826a9c815594893602ee3d0b3f0ca24d8ec7195
null-kryptonian <57069206+null-kryptonian@users.noreply.github.com>
2020-06-30 23:13:24 +0600

    Added firebase web api

---
+++ App/food_app/lib/src/widgets/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String btnText;
+
+
+  Button({this.btnText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.blueAccent,
+        child: Center(
+          child: Text(
+            "$btnText",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),);
+  }
+}
